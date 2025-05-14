999c3fb3e3ba7f75adfaa60a6a4aa47eb03c4f21
AncientStatue <glebusachev7@gmail.com>
2025-05-14 16:26:47 +0300

    -

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/sort/SortTypeUI.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/sort/SortTypeUI.java
@@ -2,41 +2,47 @@ package ru.yandex.practicum.contacts.presentation.sort;
 
 import androidx.annotation.NonNull;
 
+import ru.yandex.practicum.contacts.presentation.base.ListDiffInterface;
 import ru.yandex.practicum.contacts.presentation.sort.model.SortType;
 
-public class SortTypeUI {
+public class SortTypeUI implements ListDiffInterface<SortTypeUI> {
 
-    private final SortType sortType;
-    private final boolean selected;
+	private final SortType sortType;
+	private final boolean selected;
 
-    public SortTypeUI(@NonNull SortType sortType, boolean selected) {
-        this.sortType = sortType;
-        this.selected = selected;
-    }
+	public SortTypeUI(@NonNull SortType sortType, boolean selected) {
+		this.sortType = sortType;
+		this.selected = selected;
+	}
 
-    public SortType getSortType() {
-        return sortType;
-    }
+	public SortType getSortType() {
+		return sortType;
+	}
 
-    public boolean isSelected() {
-        return selected;
-    }
+	public boolean isSelected() {
+		return selected;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+	@Override
+	public boolean theSameAs(SortTypeUI sortTypeUI) {
+		return this.getSortType() == sortTypeUI.getSortType();
+	}
 
-        SortTypeUI that = (SortTypeUI) o;
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
 
-        if (selected != that.selected) return false;
-        return sortType == that.sortType;
-    }
+		SortTypeUI that = (SortTypeUI) o;
 
-    @Override
-    public int hashCode() {
-        int result = sortType.hashCode();
-        result = 31 * result + (selected ? 1 : 0);
-        return result;
-    }
+		if (selected != that.selected) return false;
+		return sortType == that.sortType;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = sortType.hashCode();
+		result = 31 * result + (selected ? 1 : 0);
+		return result;
+	}
 }
